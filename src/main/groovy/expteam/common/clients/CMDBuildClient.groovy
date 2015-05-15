a76b37c46aacd8f4a6fba1e3296e2c5ae7b51be0
Hubbitus <pahan@hubbitus.info>
2015-05-15 17:54:21 +0300

    EXPDEV-11 Base client, doc and test ready. Gradle project.
    Change-Id: I2d6d8933287bc37e383cfccde7d785b6582ab6ca

---
+++ src/main/groovy/expteam/common/clients/CMDBuildClient.groovy
@@ -0,0 +1,120 @@
+package expteam.common.clients
+
+import groovy.transform.TupleConstructor
+//@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='[0.7,)')
+import groovyx.net.http.RESTClient
+import groovyx.net.http.Method
+
+import static groovyx.net.http.ContentType.JSON
+
+/**
+ * Basic CMDBuild groovy client.
+ * Example of usage:
+ *
+ * CMDBuildClient client = new CMDBuildClient('http://192.168.100.183:8280/cmdbuild/services/rest/v1/', 'login', 'password');
+ * def res = client.get(path: 'lookup_types')
+ * println "Operation status: ${res.status}"
+ * println "Operation return data: ${res.data}"
+ * …
+ * client.get('classes')
+ * client.post(…)
+ *
+ * Advanced filtering and limiting supported:
+ * def res = client.get(
+ *	path: 'classes/Node/cards/'
+ * ,query: [
+ *		limit: 100 // 100 elements
+ *		,start: 10 // Starting from 10 (pagination)
+ * //	Text of JSON supported
+ * //	,filter: '{"attribute":{"simple":{"attribute":"databaseType","operator":"equal","value":[470],"parameterType":"fixed"}}}'
+ * //	Or may be built on the fly:
+ *		,filter: [
+ *			attribute:[
+ *				simple: [
+ *					attribute: 'databaseType'
+ *					,operator: 'equal'
+ *					,value: [470]
+ *					,parameterType: 'fixed'
+ *				]
+ *			]
+ *		]
+ *		// RGC with MSSQL
+ *		,filter: '{"attribute":{"and":[{"simple":{"value":[472],"attribute":"databaseType","parameterType":"fixed","operator":"equal"}},{"simple":{"value":[24],"attribute":"nodeType","parameterType":"fixed","operator":"equal"}}]}}'
+ *	]
+ * );
+ *
+ * @author Pavel Alexeev <Pahan@Hubbitus.info>
+ * @created 27.04.2015 13:51:50
+ **/
+@TupleConstructor
+class CMDBuildClient{
+	String url
+	String user
+	String pass
+
+	private @Lazy String sessionId = auth();
+	/**
+	 * 	http://192.168.100.183:8280/cmdbuild/services/rest/v1/ -> "cmdbuild/services/rest/v1/" constant, so omit
+ 	 */
+	private @Lazy String baseUriPath = new URL(url).path;
+
+	private @Lazy RESTClient client = new RESTClient(url, JSON);
+
+	/**
+	* Authenticate with given user/pass and store sessionId (and return it)
+	*
+	* @TODO add exceptions handling
+	* @return got sessionId (resp.data.data._id)
+	*/
+	private String auth(){
+		def resp = client.post(
+			path: baseUriPath + 'sessions',
+			body: [ username: user, password: pass ]
+		)
+		resp.data.data._id;
+	}
+
+	/**
+	 * Base query service method
+	 *
+	 * You do not need pass any authentication data or headers - thy are handled automatically.
+	 * But you must check exceptions and status codes
+	 *
+	 * @param params named parameters - see {@link groovyx.net.http.HTTPBuilder.RequestConfigDelegate#setPropertiesFromMap(Map)}
+	 *	In most cases only path required. If it starts from / - treated as absolute and used as is. Otherwise prepended
+	 *	by {@see baseUriPath}
+	 */
+	def query(Method method, Map params){
+		if (!params.headers){
+			params.headers = [:]
+		}
+		params.headers << [
+			'CMDBuild-Authorization': sessionId
+		];
+
+		if ('/' != params.path[0])
+			params.path = baseUriPath + params.path;
+
+		client."${method.name().toLowerCase()}"(params);
+	}
+
+	/**
+	 * Convenient method to perform GET requests
+	 *
+	 * @param params named parameters - see {@link groovyx.net.http.HTTPBuilder.RequestConfigDelegate#setPropertiesFromMap(Map)}
+	 * @return
+	 */
+	def get(Map params){
+		query(Method.GET, params)
+	}
+
+	/**
+	 * Convenient method to perform GET requests
+	 *
+	 * @param params named parameters - see {@link groovyx.net.http.HTTPBuilder.RequestConfigDelegate#setPropertiesFromMap(Map)}
+	 * @return
+	 */
+	def post(Map params){
+		query(Method.POST, params)
+	}
+}
---
+++ src/test/groovy/expteam/common/clients/CMDBuildClientTest.groovy
@@ -0,0 +1,95 @@
+package expteam.common.clients
+
+import groovyx.net.http.Method
+import org.apache.http.client.ClientProtocolException
+import spock.lang.Shared
+import spock.lang.Specification
+
+//@Grapes([
+//	@Grab(group='org.spockframework', module='spock-core', version='1.0-groovy-2.4'),
+//	@Grab(group='junit', module='junit', version='4.12')
+//])
+
+/**
+ *
+ *
+ * @author Pavel Alexeev - <Pahan@Hubbitus.info>
+ * @created 27-04-2015 11:31 AM
+ **/
+class CMDBuildClientTest extends Specification {
+	@Shared CMDBuildClient client = new CMDBuildClient('http://192.168.100.206:8080/cmdbuild/services/rest/v1/', 'admin', 'test');
+
+	def "Auth"(){
+		CMDBuildClient badClient = new CMDBuildClient('http://192.168.100.206:8080/cmdbuild/services/rest/v1/', 'admin', 'WRONG PASS');
+		when:
+			badClient.get(path: 'lookup_types')
+		then:
+			thrown(ClientProtocolException)
+
+		when:
+			client.get(path: 'lookup_types')
+		then:
+			notThrown(ClientProtocolException)
+			client.sessionId
+	}
+
+	def "Query"() {
+		when:
+			def res = client.query(Method.GET, [path: 'lookup_types'])
+		then:
+			res.data
+			res.data.data.size() > 0
+			res.data.data.size() == res.data.meta.total
+	}
+
+	def "Get"() {
+		when:
+			def res = client.get(path: 'lookup_types')
+		then:
+			res.data
+			res.data.data.size() > 0
+			res.data.data.size() == res.data.meta.total
+	}
+
+	def "Get with advanced pagination and filtering"(){
+		expect:
+			res.data
+			res.data.data.size() == 10
+			res.data.meta.total >= res.data.data.size()
+
+		where:
+			res << [
+				client.get( // filterStringSimple
+					path: 'classes/Node/cards/'
+					,query: [
+						limit: 10
+						,start: 10
+						// Variant with JSON-string filter: ESB servers
+						,filter: '{"attribute":{"simple":{"value":[23],"attribute":"nodeType","parameterType":"fixed","operator":"equal"}}}'
+					]
+				)
+				,client.get( // filterStructured
+					path: 'classes/Node/cards/'
+					,query: [
+						limit: 10
+						,start: 10
+						,filter: [
+							attribute:[
+								simple: [
+									attribute: 'nodeType'
+									,operator: 'equal'
+									,value: [23]
+									,parameterType: 'fixed'
+								]
+							]
+						]
+					]
+				)
+			]
+	}
+
+// Post used in Auth and mostly destructive, so do not test explicit
+//	def "Post"() {
+//
+//	}
+}
